ea2c2aabb1e4e7772dd30b0171abfc14a0ad7774
Yannik Sander <yannik@floxdev.com>
2022-04-08 16:01:08 +0200

    Fix flakeref

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor?ref=ysndr";
+  inputs.capacitor.url = "github:flox/capacitor?ref=ysndr";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, capacitor, flake-utils }:
