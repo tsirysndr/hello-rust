cdce736f9d5c2b60e5c294a6c34be94f233d32b8
Yannik Sander <yannik@floxdev.com>
2022-09-21 18:10:52 +0200

    Use tng style template

--- flake.nix
+++ flake.nix
@@ -1,42 +1,19 @@
 {
-  description = "Hello in Rust";
+  
 
-  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor?ref=v0";
-  inputs.capacitor.inputs.root.follows = "/";
-  inputs.capacitor.inputs.nixpkgs.follows = "nixpkgs/nixpkgs-stable";
+  # Declaration of external resources
+  # =================================
 
-  inputs.nixpkgs.url = "git+ssh://git@github.com/flox/nixpkgs-flox";
-  inputs.nixpkgs.inputs.capacitor.follows = "capacitor";
+  # =================================
 
-  inputs.floxpkgs.url = "git+ssh://git@github.com/flox/floxpkgs";
-  inputs.floxpkgs.inputs.capacitor.follows = "capacitor";
-  inputs.floxpkgs.inputs.nixpkgs.follows = "nixpkgs";
 
+  description = "Floxpkgs/Project Template";
   nixConfig.bash-prompt = "[flox] \\[\\033[38;5;172m\\]λ \\[\\033[0m\\]";
 
-  outputs = args @ {capacitor, ...}:
-    capacitor args (
-      {
-        inputs,
-        lib,
-        ...
-      }: {
-        devShells.default =
-          lib.optionalAttrs
-          (builtins.pathExists ./flox.toml)
-          (inputs.floxpkgs.lib.mkFloxShell ./flox.toml {});
-
-        config = {
-          stabilities = {
-            stable = inputs.nixpkgs.stable;
-            staging = inputs.nixpkgs.staging;
-            unstable = inputs.nixpkgs.unstable;
-            default = inputs.nixpkgs.stable;
-          };
-          extraPlugins = [
-            (inputs.capacitor.plugins.allLocalResources {})
-          ];
-        };
-      }
-    );
+  # Template DO NOT EDIT
+  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  # TODO: injected by the cli, or used via registry?
+  inputs.floxpkgs.url = "github:flox/floxpkgs";
+  outputs = args @ {floxpkgs, ...}: floxpkgs.capacitor args (import ./flox.nix);
+  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
